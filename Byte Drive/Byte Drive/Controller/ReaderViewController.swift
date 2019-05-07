96d93b4ea47e4c91a2de34776f9618aa154a0fb3
Jon-Jon <j2naraja@gmail.com>
2019-05-07 16:53:42 -0700

    working on download and view button.
    added boolean var for view, and a new ReaderViewController

---
+++ Byte Drive/Byte Drive/Controller/ReaderViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ReaderViewController.swift
+//  Byte Drive
+//
+//  Created by Jonathan Naraja on 5/7/19.
+//  Copyright © 2019 Dale Christian Seen. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+
+class ReaderViewController: UIViewController {
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    var downloadURL:String!
+}
--- Byte Drive/Byte Drive/View/Custom Cells/DescriptionDownloadTableViewCell.swift
+++ Byte Drive/Byte Drive/View/Custom Cells/DescriptionDownloadTableViewCell.swift
@@ -7,19 +7,32 @@
 //
 
 import UIKit
+import Firebase
 
 class DescriptionDownloadTableViewCell: UITableViewCell {
     
+    var downloadComplete = false
+    var downloadURL = String()
     var fileStorageRef: String = String()
     @IBOutlet weak var downloadButton: UIButton!
+    @IBOutlet weak var viewButton: UIButton!
+    
     
     override func awakeFromNib() {
+        
         super.awakeFromNib()
         
         downloadButton.layer.cornerRadius = 5
         downloadButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
         downloadButton.layer.backgroundColor = #colorLiteral(red: 0.4235294118, green: 0.3607843137, blue: 0.9058823529, alpha: 1)
         downloadButton.layer.borderColor = #colorLiteral(red: 0.3053061664, green: 0.2611849308, blue: 0.6665206552, alpha: 1)
+        
+        
+        viewButton.layer.cornerRadius = 5
+        viewButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        viewButton.layer.backgroundColor = #colorLiteral(red: 0.4235294118, green: 0.3607843137, blue: 0.9058823529, alpha: 1)
+        viewButton.layer.borderColor = #colorLiteral(red: 0.3053061664, green: 0.2611849308, blue: 0.6665206552, alpha: 1)
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -28,8 +41,23 @@ class DescriptionDownloadTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    @IBAction func pressedView(_ sender: Any) {
+        // TODO: view file
+        if (downloadComplete == true) {
+            print("view button pressed")
+        }
+        else {
+            print("Download not pressed")
+        }
+    }
     @IBAction func pressedDownload(_ sender: Any) {
         // TODO: Do download here:
         // fileStorageRef contains the reference path of the file in firebase storage
+        
+        // After download is complete, enable view button
+        //print("Download button pressed")
+        //downloadComplete = true
+        let dRef = Database.database().reference()
+        
     }
 }
