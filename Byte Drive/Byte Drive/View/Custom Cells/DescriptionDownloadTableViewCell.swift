92b64ee91f4e8d7160c91d02f32d1009f416efe5
Jon-Jon <j2naraja@gmail.com>
2019-05-12 21:00:14 -0700

    m

--- Byte Drive/Byte Drive/View/Custom Cells/DescriptionDownloadTableViewCell.swift
+++ Byte Drive/Byte Drive/View/Custom Cells/DescriptionDownloadTableViewCell.swift
@@ -15,11 +15,6 @@ class DescriptionDownloadTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        
-        downloadButton.layer.cornerRadius = 5
-        downloadButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
-        downloadButton.layer.backgroundColor = #colorLiteral(red: 0.4235294118, green: 0.3607843137, blue: 0.9058823529, alpha: 1)
-        downloadButton.layer.borderColor = #colorLiteral(red: 0.3053061664, green: 0.2611849308, blue: 0.6665206552, alpha: 1)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
